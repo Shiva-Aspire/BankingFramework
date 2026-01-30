e914c9f783f1e591d684b5f18f2c9eeefa122529
Shiva Amballa <amballashiva967@gmail.com>
2026-01-30 09:56:13 +0530

    added new end to end test case in the DashboardTest and also added smoke group execution xml

--- src/main/java/DashboardPage.java
+++ src/main/java/DashboardPage.java
@@ -20,15 +20,64 @@ public class DashboardPage extends Utils{
     @FindBy(css="#transactions-count-title")
     WebElement totalTransactions;
 
+    @FindBy(xpath = "//a[@id='add-account-link']")
+    WebElement addAccountbtn;
+
+    @FindBy(xpath = "//input[@id='account-name']")
+    WebElement accountName;
+
+    @FindBy(xpath = "//button[@id='account-type']")
+    WebElement selectAccountType;
+
+    @FindBy(xpath = "//div[normalize-space()='Savings Account']")
+    WebElement savingsAccountDd;
+
+    @FindBy(xpath = "//input[@id='initial-balance']")
+    WebElement initalBal;
+
+    @FindBy(xpath = "//button[@id='status-active']")
+    WebElement statuActiveRBtn;
+
+    @FindBy(css = "#overdraft-label")
+    WebElement enableOverdraftProtectionCheckBox;
+
+    @FindBy(xpath = "//button[@id='save-account-btn']")
+    WebElement saveAccountBtn;
+
     //*************Actions***************
     public String getActiveAccountTxt(){
         click(tabdashBoard);
         return getText(activeAccount);
+
     }
     public String getTotalTransactionTxt(){
         click(tabdashBoard);
         return getText(totalTransactions);
     }
+    public void clickOnAddAccountQuickAction(){
+        click(addAccountbtn);
+    }
+    public void clickOnAccountName(){
+        click(accountName);
+    }
+    public void clickOnSelectAccountType(){
+        click(selectAccountType);
+        jsClick(savingsAccountDd);
+    }
+    public void initalBal(){
+        click(initalBal);
+    }
+    public void clickonStatusActiveRBtn(){
+        click(statuActiveRBtn);
+    }
+    public void clickOnEnableProtectionCheckbox(){
+        click(enableOverdraftProtectionCheckBox);
+    }
+    public void clickOnSaveAccountBtn(){
+        click(saveAccountBtn);
+    }
+
+
 
 
 
--- src/test/java/DashboardTest.java
+++ src/test/java/DashboardTest.java
@@ -1,15 +1,30 @@
-
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class DashboardTest extends BaseClass{
 
     @Test(dataProvider = "validLoginData")
-    public void verifyDashboardPage(String userName, String password){
+    public void verifyDashboardTopPage(String userName, String password){
         LoginPage lp=new LoginPage(driver);
         lp.clickOnLoginBtn(userName, password);
         DashboardPage dp=new DashboardPage(driver);
         Assert.assertEquals(dp.getActiveAccountTxt(),GlobalProperties.getExpvalues("activeAccount.txt"));
         Assert.assertEquals(dp.getTotalTransactionTxt(),GlobalProperties.getExpvalues("totalTransaction.txt"));
+    }
+    //End-to-End test Automation Script for creating a new account
+    @Test(groups = "smoke", dataProvider = "validLoginData")
+    public void verifyAddAccountQuickAction(String userName, String password) {
+        LoginPage lp=new LoginPage(driver);
+        lp.clickOnLoginBtn(userName, password);
+        DashboardPage dp=new DashboardPage(driver);
+        dp.clickOnAddAccountQuickAction();
+        dp.clickOnAccountName();
+        dp.accountName.sendKeys(GlobalProperties.getExpvalues("accountName.txt"));
+        dp.clickOnSelectAccountType();
+        dp.initalBal();
+        dp.initalBal.sendKeys(GlobalProperties.getExpvalues("initalBalinput.txt"));
+        dp.clickonStatusActiveRBtn();
+        dp.clickOnEnableProtectionCheckbox();
+        dp.clickOnSaveAccountBtn();
     }
 }
