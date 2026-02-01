cd006945308dbc43025ffcdf733fd6205428f413
Shiva Amballa <amballashiva967@gmail.com>
2026-02-01 18:27:21 +0530

    added new test case in the loginTest class

--- src/test/java/com/bank/tests/LoginTest.java
+++ src/test/java/com/bank/tests/LoginTest.java
@@ -1,5 +1,6 @@
 package com.bank.tests;
 
+import com.bank.Utility.GlobalProperties;
 import com.bank.pom.LoginPage;
 import org.openqa.selenium.By;
 import org.testng.Assert;
@@ -11,6 +12,7 @@ public class LoginTest extends BaseClass {
     public void loginWithValidCreds(String userName, String password) {
         LoginPage lp = new LoginPage(driver);
         lp.clickOnLoginBtn(userName,password);
+        driver.findElement(By.tagName("a"));
     }
 
     @Test(groups = {"regression"}, dataProvider = "InvalidLoginData")
@@ -20,4 +22,11 @@ public class LoginTest extends BaseClass {
         String alertText = driver.findElement(By.cssSelector("#alert-message")).getText();
         Assert.assertEquals(alertText,"⚠️ Invalid username or password. Please try again.");
     }
+    @Test(groups = {"regression"})
+    public void verifyloginHomeBtn(){
+       LoginPage lp = new LoginPage(driver);
+       lp.clickHomeBtn();
+       Assert.assertEquals(lp.getCoffeeImgTxt(), GlobalProperties.getExpvalues("paragraph.txt"));
+    }
+
 }
